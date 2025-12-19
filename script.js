febe4ebfc2eb0948fc00f00db2413bd11a1f78c3
IgorRozanov <rozanovil@gmail.com>
2025-12-19 16:15:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let cities = [];
+let person = [];
+let specialization = [];
+
+
+Promise.all(
+    [
+        fetch('cities.json'),
+        fetch('person.json'),
+        fetch('specializations.json'),
+    ]
+).then(async ([citiesResponse, personResponse, specializationResponse]) => {
+    const citiesJson = await citiesResponse.json();
+    const personJson = await personResponse.json();
+    const specializationJson = await specializationResponse.json();
+    return [citiesJson, personJson, specializationJson]
+
+})
+
+    .then(response => {
+        cities = response[0];
+        person = response[1];
+        specialization = response[2];
+
+    })
+console.log(cities)
+
+//
+// function getInfo() {
+//     // console.log(person);
+//     let output = person.map(item => {
+//         let citi = cities.find(citiItem => {
+//
+//             return citiItem.id === item.personal.locationId;
+//
+//         });
+//         item.personal.locationId = citi.name
+//         return item.personal
+//     })
+//     console.log(output[0].firstName)
+// }
+
+
+
