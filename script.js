3a7209f047867a538f5bb11de33121566dd07d00
IgorRozanov <rozanovil@gmail.com>
2025-12-29 11:21:19 +0300

    filter backenders

--- script.js
+++ script.js
@@ -21,14 +21,17 @@ Promise.all(
         cities = response[0];
         person = response[1];
         specialization = response[2];
-            getInfo.call(person[0]);
+        getInfo.call(person[0]);
         console.log(`разработчик на React `);
         findFirstReact();
+        findAllFigma();
+        isOlder(25);
+        findBackend();
 
 
     })
 
-function getSkillUser (user, skillName) {
+function getSkillUser(user, skillName) {
     return user.skills.find(skill => skill.name.toLowerCase() === skillName);
 }
 
@@ -40,13 +43,60 @@ function getInfo() {
     }
 }
 
-function findFirstReact () {
-    const userReact = person.find(item=> {
-        return getSkillUser(item, 'figma')
+function findFirstReact() {
+    const userReact = person.find(item => {
+        return getSkillUser(item, 'react')
     });
     console.log(`userReact ${userReact}`)
     getInfo.call(userReact);
 
+}
+
+function findAllFigma() {
+    console.log('knows Figma')
+    person.forEach(person => {
+        let skillItem = person.skills.find(item => {
+            return item.name.toLowerCase() === 'figma'
+        })
+        if (skillItem) {
+
+            getInfo.call(person);
+        }
+    })
+}
+
+function isOlder(age) {
+    console.log(`is older ${age}`)
+    const currentDate = new Date();
+    person.forEach(person => {
+        let personage = person.personal.birthday
+        let dateParts = personage.split('.');
+        let personBirthday = new Date(+dateParts[2], +dateParts[1], +dateParts[0]);
+        let older = Math.abs(currentDate.getFullYear() - personBirthday.getFullYear()) > age;
+        if (older) {
+            getInfo.call(person);
+        }
+    })
+
+
+}
+
+
+function findBackend() {
+    console.log('backenders')
+    let backendId = specialization.find(item => item.name.toLowerCase() === 'backend');
+    if (backendId) {
+        let backenders = person.filter(item => item.personal.specializationId === backendId.id);
+        backenders.forEach(person => {
+            let occupation = person.request.find(item => {
+                return item.value.toLowerCase() === 'полная';
+            })
+            if (occupation) {
+                getInfo.call(person);
+            }
+
+        })
+    }
 }
 
 
